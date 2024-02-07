1cd821a13fb28347467a142875b74f516f5b81c8
Maksandre <maksandre@yandex.ru>
2024-02-07 16:02:51 +0400

    test: login by enter

--- tests/login-positive.spec.ts
+++ tests/login-positive.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "fixtures/base-fixture";
 
 for (const login of ["email", "username"] as const) {
-  test(`User can login with correct ${login} and password`, async ({
+  test(`User can login with correct ${login} and password clicking by "Sign In" button`, async ({
     loginPage,
     dashboardPage,
     user,
@@ -13,6 +13,23 @@ for (const login of ["email", "username"] as const) {
     await loginPage.passwordInput.fill(user.password);
     await loginPage.signInButton.click();
 
+    // Dashboard page only visible to authorized users
     await dashboardPage.waitLoaded();
   });
 }
+
+test("User can login with correct credentials by Enter", async ({
+  loginPage,
+  dashboardPage,
+  user,
+}) => {
+  await loginPage.goto();
+  await loginPage.waitLoaded();
+
+  await loginPage.loginInput.fill(user.email);
+  await loginPage.passwordInput.fill(user.password);
+  await loginPage.passwordInput.pressEnter();
+
+  // Dashboard page only visible to authorized users
+  await dashboardPage.waitLoaded();
+});
